1b977f236bb12745f4628072ba4a96503d3969f6
Skierkowski Grzegorz <42655090+gskierk@users.noreply.github.com>
2026-03-14 17:59:38 +0100

    fix: add missing base to Syllabi.svelte

--- src/lib/components/sections/Syllabi.svelte
+++ src/lib/components/sections/Syllabi.svelte
@@ -1,4 +1,8 @@
 <!-- eslint-disable svelte/no-navigation-without-resolve -->
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <section id="sylabusy" class="section">
   <h2>Sylabusy</h2>
   <p>Poniżej znajdziesz sylabusy, pytania, odpowiedzi i wytyczne do akredytacji:</p>
@@ -6,50 +10,57 @@
     <li>
       <strong>Certyfikowany Tester - Poziom Podstawowy v4.0.1</strong>
       <ul>
-        <li><a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-sylabus.pdf" download>Sylabus</a></li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-A-pytania-v1.7.pdf" download
+          <a href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-sylabus.pdf" download>Sylabus</a>
+        </li>
+        <li>
+          <a href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-A-pytania-v1.7.pdf" download
             >Pytania - zestaw A</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-A-odpowiedzi-v1.7.pdf" download
-            >Odpowiedzi - zestaw A</a
+          <a
+            href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-A-odpowiedzi-v1.7.pdf"
+            download>Odpowiedzi - zestaw A</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-B-pytania-v1.7.pdf" download
+          <a href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-B-pytania-v1.7.pdf" download
             >Pytania - zestaw B</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-B-odpowiedzi-v1.7.pdf" download
-            >Odpowiedzi - zestaw B</a
+          <a
+            href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-B-odpowiedzi-v1.7.pdf"
+            download>Odpowiedzi - zestaw B</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-C-pytania-v1.6.pdf" download
+          <a href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-C-pytania-v1.6.pdf" download
             >Pytania - zestaw C</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-C-odpowiedzi-v1.6.pdf" download
-            >Odpowiedzi - zestaw C</a
+          <a
+            href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-C-odpowiedzi-v1.6.pdf"
+            download>Odpowiedzi - zestaw C</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-D-pytania-v1.5.pdf" download
+          <a href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-D-pytania-v1.5.pdf" download
             >Pytania - zestaw D</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-D-odpowiedzi-v1.5.pdf" download
-            >Odpowiedzi - zestaw D</a
+          <a
+            href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-D-odpowiedzi-v1.5.pdf"
+            download>Odpowiedzi - zestaw D</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-wytyczne-do-akredytacji-v0.3.pdf" download
-            >Wytyczne do akredytacji</a
+          <a
+            href="{base}/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-wytyczne-do-akredytacji-v0.3.pdf"
+            download>Wytyczne do akredytacji</a
           >
         </li>
       </ul>
@@ -57,25 +68,36 @@
     <li>
       <strong>Certyfikowany Tester - Poziom Zaawansowany - Analityk Testów v4.0</strong>
       <ul>
-        <li><a href="/documents/syllabi/ta-4.0/TA-4.0-PL-sylabus.pdf" download>Sylabus</a></li>
-        <li><a href="/documents/syllabi/ta-4.0/TA-4.0-PL-pytania.pdf" download>Pytania</a></li>
-        <li><a href="/documents/syllabi/ta-4.0/TA-4.0-PL-odpowiedzi.pdf" download>Odpowiedzi</a></li>
+        <li>
+          <a href="{base}/documents/syllabi/ta-4.0/TA-4.0-PL-sylabus.pdf" download>Sylabus</a>
+        </li>
+        <li>
+          <a href="{base}/documents/syllabi/ta-4.0/TA-4.0-PL-pytania.pdf" download>Pytania</a>
+        </li>
+        <li>
+          <a href="{base}/documents/syllabi/ta-4.0/TA-4.0-PL-odpowiedzi.pdf" download>Odpowiedzi</a>
+        </li>
       </ul>
     </li>
     <li>
       <strong>Certyfikowany Tester - Poziom Zaawansowany - Zarządzanie Testami v3.0</strong>
       <ul>
-        <li><a href="/documents/syllabi/tm-3.0/TM-3.0-PL-sylabus.pdf" download>Sylabus</a></li>
         <li>
-          <a href="/documents/syllabi/tm-3.0/TM-3.0-PL-pytania-v1.3.3.pdf" download>Pytania</a>
+          <a href="{base}/documents/syllabi/tm-3.0/TM-3.0-PL-sylabus.pdf" download>Sylabus</a>
         </li>
         <li>
-          <a href="/documents/syllabi/tm-3.0/TM-3.0-PL-odpowiedzi-v1.3.3.pdf" download>Odpowiedzi</a
+          <a href="{base}/documents/syllabi/tm-3.0/TM-3.0-PL-pytania-v1.3.3.pdf" download>Pytania</a
           >
         </li>
         <li>
-          <a href="/documents/syllabi/tm-3.0/TM-3.0-PL-wytyczne-do-akredytacji-v1.0.pdf" download
-            >Wytyczne do akredytacji</a
+          <a href="{base}/documents/syllabi/tm-3.0/TM-3.0-PL-odpowiedzi-v1.3.3.pdf" download
+            >Odpowiedzi</a
+          >
+        </li>
+        <li>
+          <a
+            href="{base}/documents/syllabi/tm-3.0/TM-3.0-PL-wytyczne-do-akredytacji-v1.0.pdf"
+            download>Wytyczne do akredytacji</a
           >
         </li>
       </ul>
