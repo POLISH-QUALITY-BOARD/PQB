c9b8578c7fee7e05be94d0e4c6bf496a95c371ea
Skierkowski Grzegorz <42655090+gskierk@users.noreply.github.com>
2026-03-14 17:57:08 +0100

    feat: add syllabi section and rename certification to portfolio

---
+++ src/lib/components/sections/Syllabi.svelte
@@ -0,0 +1,84 @@
+<!-- eslint-disable svelte/no-navigation-without-resolve -->
+<section id="sylabusy" class="section">
+  <h2>Sylabusy</h2>
+  <p>Poniżej znajdziesz sylabusy, pytania, odpowiedzi i wytyczne do akredytacji:</p>
+  <ul>
+    <li>
+      <strong>Certyfikowany Tester - Poziom Podstawowy v4.0.1</strong>
+      <ul>
+        <li><a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-sylabus.pdf" download>Sylabus</a></li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-A-pytania-v1.7.pdf" download
+            >Pytania - zestaw A</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-A-odpowiedzi-v1.7.pdf" download
+            >Odpowiedzi - zestaw A</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-B-pytania-v1.7.pdf" download
+            >Pytania - zestaw B</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-B-odpowiedzi-v1.7.pdf" download
+            >Odpowiedzi - zestaw B</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-C-pytania-v1.6.pdf" download
+            >Pytania - zestaw C</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-C-odpowiedzi-v1.6.pdf" download
+            >Odpowiedzi - zestaw C</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-D-pytania-v1.5.pdf" download
+            >Pytania - zestaw D</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-zestaw-D-odpowiedzi-v1.5.pdf" download
+            >Odpowiedzi - zestaw D</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/fl-4.0.1/FL-4.0.1-PL-wytyczne-do-akredytacji-v0.3.pdf" download
+            >Wytyczne do akredytacji</a
+          >
+        </li>
+      </ul>
+    </li>
+    <li>
+      <strong>Certyfikowany Tester - Poziom Zaawansowany - Analityk Testów v4.0</strong>
+      <ul>
+        <li><a href="/documents/syllabi/ta-4.0/TA-4.0-PL-sylabus.pdf" download>Sylabus</a></li>
+        <li><a href="/documents/syllabi/ta-4.0/TA-4.0-PL-pytania.pdf" download>Pytania</a></li>
+        <li><a href="/documents/syllabi/ta-4.0/TA-4.0-PL-odpowiedzi.pdf" download>Odpowiedzi</a></li>
+      </ul>
+    </li>
+    <li>
+      <strong>Certyfikowany Tester - Poziom Zaawansowany - Zarządzanie Testami v3.0</strong>
+      <ul>
+        <li><a href="/documents/syllabi/tm-3.0/TM-3.0-PL-sylabus.pdf" download>Sylabus</a></li>
+        <li>
+          <a href="/documents/syllabi/tm-3.0/TM-3.0-PL-pytania-v1.3.3.pdf" download>Pytania</a>
+        </li>
+        <li>
+          <a href="/documents/syllabi/tm-3.0/TM-3.0-PL-odpowiedzi-v1.3.3.pdf" download>Odpowiedzi</a
+          >
+        </li>
+        <li>
+          <a href="/documents/syllabi/tm-3.0/TM-3.0-PL-wytyczne-do-akredytacji-v1.0.pdf" download
+            >Wytyczne do akredytacji</a
+          >
+        </li>
+      </ul>
+    </li>
+  </ul>
+</section>
