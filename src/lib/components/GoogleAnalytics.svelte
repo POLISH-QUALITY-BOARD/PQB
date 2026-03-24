b1aa80d79c7c3c78da71349f084e5abce65044e3
Skierkowski Grzegorz <42655090+gskierk@users.noreply.github.com>
2026-03-24 11:00:55 +0100

    fix: fix GoogleAnalytics.svelte

--- src/lib/components/GoogleAnalytics.svelte
+++ src/lib/components/GoogleAnalytics.svelte
@@ -1,33 +1,21 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { browser } from '$app/environment';
   import { PUBLIC_GA_ID } from '$env/static/public';
 
-  onMount(() => {
+  if (browser) {
     window.dataLayer = window.dataLayer || [];
-
-    function gtag(...args: unknown[]) {
-      window.dataLayer.push(args);
-    }
-
-    window.gtag = gtag;
-
-    gtag('consent', 'default', {
-      analytics_storage: 'denied'
-    });
-
-    gtag('js', new Date());
-    gtag('config', PUBLIC_GA_ID);
-
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${PUBLIC_GA_ID}`;
-
-    document.head.appendChild(script);
-  });
+    window.gtag = function () {
+      window.dataLayer.push(arguments);
+    };
+    window.gtag('consent', 'default', { analytics_storage: 'denied' });
+    window.gtag('js', new Date());
+    window.gtag('config', PUBLIC_GA_ID);
+  }
 </script>
 
 <svelte:head>
   <link rel="preconnect" href="https://www.googletagmanager.com" />
   <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
   <link rel="dns-prefetch" href="https://google-analytics.com" />
+  <script async src="https://www.googletagmanager.com/gtag/js?id={PUBLIC_GA_ID}"></script>
 </svelte:head>
