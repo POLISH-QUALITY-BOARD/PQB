07f5c71b660ef6679844f81b08e3e11f83de4548
Skierkowski Grzegorz <42655090+gskierk@users.noreply.github.com>
2026-03-31 00:05:00 +0200

    test: strengthen test in Section.svelte.spec.ts

--- src/lib/components/Section.svelte.spec.ts
+++ src/lib/components/Section.svelte.spec.ts
@@ -8,18 +8,24 @@ import Section from '$lib/components/Section.svelte';
 const snippet = (html: string) => createRawSnippet(() => ({ render: () => html }));
 
 describe('Section.svelte', () => {
-  test('renders section with id, heading and body', async () => {
+  test('renders section with class, id, heading and body', async () => {
+    const className = 'bg-white';
     const id = faker.string.uuid();
     const heading = faker.lorem.words(3);
     const content = faker.lorem.sentence();
 
     await render(Section, {
+      class: className,
       id,
       heading: snippet(`<span>${heading}</span>`),
       children: snippet(`<p>${content}</p>`)
     });
 
-    await expect.element(page.getByTestId('section')).toHaveAttribute('id', id);
+    const expectSection = expect.element(page.getByTestId('section'));
+
+    await expectSection.toHaveClass(className);
+    await expectSection.toHaveAttribute('id', id);
+
     await expect.element(page.getByTestId('section-heading')).toHaveTextContent(heading);
     await expect.element(page.getByTestId('section-body')).toHaveTextContent(content);
   });
