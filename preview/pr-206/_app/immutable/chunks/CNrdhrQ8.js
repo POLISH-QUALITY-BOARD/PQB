389c84e62de98408d2d1acf2914634f356599e97
gskierk <gskierk@users.noreply.github.com>
2026-07-14 21:34:09 +0000

    Deploying to gh-pages from @ POLISH-QUALITY-BOARD/PQB@109e14f255d113230036e803650a69602edf995a 🚀

---
+++ preview/pr-206/_app/immutable/chunks/CNrdhrQ8.js
@@ -0,0 +1,1 @@
+import{O as e,T as t,a as n,c as r}from"./Be7up4pJ.js";import"./xihTtKlq.js";var i=new Set([`$$slots`,`$$events`,`$$legacy`]),a=e(`<svg><path fill="currentColor" d="M21 7L9 19l-5.5-5.5l1.41-1.41L9 16.17L19.59 5.59z"></path></svg>`);function o(e,o){let s=n(o,i);var c=a();r(c,()=>({viewBox:`0 0 24 24`,width:`1.2em`,height:`1.2em`,...s})),t(e,c)}export{o as t};
